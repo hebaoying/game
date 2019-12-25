60632c1739f0734babce1d59134a218272e3d1e7
Baoying He <hi@hebaoying.com>
2019-12-25 23:14:11 +0800

    第八课，子类里面怎样给父类添加元素，之前一直没看懂。现在才看懂。

---
+++ 7.26/guagame/gua_scene.js
@@ -0,0 +1,26 @@
+class GuaScene {
+    constructor(game) {
+        this.game = game
+        this.elements = []
+    }
+    static new(game) {
+        var i = new this(game)
+        return i
+    }
+    addElement(img) {
+        img.scene = this
+        this.elements.push(img)
+    }
+    draw() {
+        for (let i = 0; i < this.elements.length; i++) {
+            let e = this.elements[i]
+            this.game.drawImage(e)
+        }
+    }
+    update() {
+        for (let i = 0; i < this.elements.length; i++) {
+            let e = this.elements[i]
+            e.update()
+        }
+    }
+}
